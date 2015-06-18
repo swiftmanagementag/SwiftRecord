4abc0076f37b9182cebd37c9a02a878ad41b1654
Ian G <yo.ian.g@gmail.com>
2015-06-18 09:35:27 -0400

    Convert project related files to Xcode 7

--- SwiftRecord/SwiftRecord/SwiftRecord.h
+++ SwiftRecord/SwiftRecord/SwiftRecord.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 ark. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftRecord.
 FOUNDATION_EXPORT double SwiftRecordVersionNumber;
