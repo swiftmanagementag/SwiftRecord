c3964d4396dfa78bc8259ed89c004ff83ed8536e
Graham Lancashire <F83A537F-79CE508A-B96B5FDE-C7510D25-66E46418>
2025-02-19 12:04:21 +0100

    Package fix

--- Package.swift
+++ Package.swift
@@ -16,19 +16,11 @@ let package = Package(
             targets: ["SwiftRecord"]
         ),
     ],
-	dependencies: [
-		.package(url: "https://github.com/swiftmanagementag/SwiftRecord.swift", branch: "master")
-	],
 	targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftRecord",
-			dependencies: [
-				.product(name: "SwiftRecord", package: "SwiftRecord.swift")
-			],
-			resources: [
-				.process("Resources"),
-			]),
+            name: "SwiftRecord"
+			)
 	]
 )
