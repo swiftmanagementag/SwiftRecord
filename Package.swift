c18c6d7ae00d0638c9f8408c8432442e92020ea1
Graham Lancashire <F83A537F-79CE508A-B96B5FDE-C7510D25-66E46418>
2025-02-19 14:20:11 +0100

    Rewrite

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftRecord",
-	platforms: [
-		.iOS(.v15),
-		.macCatalyst(.v14),
-		],
+    platforms: [
+        .iOS(.v15),
+        .macCatalyst(.v14),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftRecord",
-            targets: ["SwiftRecord"]
-        ),
+            targets: ["SwiftRecord"])
     ],
-	targets: [
-		.target(name: "SwiftRecord")
-	]
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftRecord"),
+        .testTarget(
+            name: "SwiftRecordTests",
+            dependencies: ["SwiftRecord"]
+        ),
+    ]
 )
